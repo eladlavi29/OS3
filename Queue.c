6bef47ce20205b510acab93244c451cd3018316f
amirb <amirbourvine@campus.technion.ac.il>
2023-06-07 21:28:43 +0300

    queue written

--- Queue.c
+++ Queue.c
@@ -127,4 +127,20 @@ void print_queue(struct Queue* q){
     }
 }
 
+int main(){
+    Queue* q = Queue_ctor();
+    pthread_t t1 = 1;
+    pthread_t t2 = 2;
+    pthread_t t3 = 3;
+    pthread_t t4 = 4;
+    pthread_t t5 = 5;
+    print_queue(q);
+    enqueue(q,t1);
+    enqueue(q,t2);
+    enqueue(q,t3);
+    enqueue(q,t4);
+    enqueue(q,t5);
+    print_queue(q);
+}
+
 #endif //OS3_QUEUE_H
