69985703e015a1e8e7dc53b69e16258fcbb635ae
amirb <amirbourvine@campus.technion.ac.il>
2023-06-09 17:14:14 +0300

    part3 work

--- request.c
+++ request.c
@@ -133,7 +133,6 @@ void requestServeDynamic(int fd, char *filename, char *cgiargs, Stats* stats, Th
 
 void requestServeStatic(int fd, char *filename, int filesize, Stats* stats, Thread* thread)
 {
-    printf("HERE2\n");
    int srcfd;
    char *srcp, filetype[MAXLINE], buf[MAXBUF];
 
@@ -171,7 +170,6 @@ void requestServeStatic(int fd, char *filename, int filesize, Stats* stats, Thre
 // handle a request
 void requestHandle(int fd, Stats* stats, ThreadManager* tm)
 {
-    printf("HERE0\n");
    int is_static;
    struct stat sbuf;
    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
@@ -206,8 +204,6 @@ void requestHandle(int fd, Stats* stats, ThreadManager* tm)
    else{
        tm->thread_arr[ind].dynamic_req_count = tm->thread_arr[ind].dynamic_req_count + 1;
    }
-
-    printf("HERE1\n");
 
    if (stat(filename, &sbuf) < 0) {
       requestError(fd, filename, "404", "Not found", "OS-HW3 Server could not find this file");
