22a2f300284eb590b7c3edf5bdbe6712f8415901
amirb <amirbourvine@campus.technion.ac.il>
2023-06-16 13:44:25 +0300

    header changed

--- request.c
+++ request.c
@@ -4,6 +4,8 @@
 
 #include "request.h"
 
+struct ThreadManager;
+
 // requestError(      fd,    filename,        "404",    "Not found", "OS-HW3 Server could not find this file");
 void requestError(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg, Stats* stats, Thread* thread)
 {
