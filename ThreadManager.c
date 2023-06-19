62a6e8c5b90f24370498475d3d7dd9c3c657c11f
amirb <amirbourvine@campus.technion.ac.il>
2023-06-19 17:44:25 +0300

    dynamic debug

--- ThreadManager.c
+++ ThreadManager.c
@@ -164,7 +164,9 @@ void ThreadManagerHandleRequest(ThreadManager* tm, int fd, Stats* stats){
     //Dynamic protocol
     if(getSize(tm->waitingRequests) + getSize(tm->busyRequests) >= tm->queue_size_dynamic
        && strcmp(tm->sched_alg, DYNAMIC_SCHEDALG) == 0){
+        printf("%s","HERE0\n");
         Close(fd);
+        printf("%s","HERE1\n");
         if(tm->queue_size_dynamic < tm->queue_size){
             tm->queue_size_dynamic = tm->queue_size_dynamic + 1;
         }
@@ -175,7 +177,7 @@ void ThreadManagerHandleRequest(ThreadManager* tm, int fd, Stats* stats){
         pthread_mutex_unlock(&tm->waitingRequests->m);
         pthread_mutex_unlock(&tm->m);
 
-        printf("%s","HERE!\n");
+        printf("%s","HERE2\n");
 
         return;
     }
