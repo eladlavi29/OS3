a51f9b501100eeb8da78b8c55fc172e5a61c2f83
amirb <amirbourvine@campus.technion.ac.il>
2023-06-19 12:36:23 +0300

    header changed

--- request.c
+++ request.c
@@ -142,14 +142,15 @@ void requestServeDynamic(int fd, char *filename, char *cgiargs, Stats* stats, Th
 
     Rio_writen(fd, buf, strlen(buf));
 
-   if (Fork() == 0) {
+    int pid = Fork();
+   if (pid == 0) {
       /* Child process */
       Setenv("QUERY_STRING", cgiargs, 1);
       /* When the CGI process writes to stdout, it will instead go to the socket */
       Dup2(fd, STDOUT_FILENO);
       Execve(filename, emptylist, environ);
    }
-   Wait(NULL);
+   WaitPid(pid,NULL);
 }
 
 
